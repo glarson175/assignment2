1e1ae1013a007d6c733c85c0927fa7663f6fdc91
glarson175 <gsl13@pitt.edu>
2016-10-05 20:24:08 -0400

    ayyyyy! it worked!
    javascript function checks for checkbox and shows/hides the fieldset
    with the radio buttons.

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 	$('#radio').hide();
+	$('#ComicLover').change(function() {
+    if(document.getElementById('ComicLover').checked) {
+    $('#radio').show();
+		} 
+		else {
+				$('#radio').hide();
+		}
+	});
 });
 $(function() { /* code here */ });
